de709c546a3d11e9d552d4bbf767b4a9ec710306
zpfhfh0124 <48612497+zpfhfh0124@users.noreply.github.com>
2024-03-26 21:58:45 +0900

    TextMesh Pro 재설치

---
+++ GT_Viewer/Assets/Scripts/Controller/PlayerListItem.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GT
+{
+    public class PlayerListItem : MonoBehaviour
+    {
+        
+
+        void Start()
+        {
+
+        }
+
+    }
+}
